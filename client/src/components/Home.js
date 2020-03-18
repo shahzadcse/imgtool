a8cece09e1b2614f5fdcab8a1eb6f3aa55f935b1
Shahzad Hussain <shahzad.hussain@informa.com>
2020-03-18 09:15:51 +0400

    Img Upload tool

---
+++ client/src/components/Home.js
@@ -0,0 +1,131 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import $ from 'jquery';
+
+class Home extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+    
+    selectedFiles: null,
+    list : []
+    }
+
+    this.multipleFileUploadHandler = this.multipleFileUploadHandler.bind(this);
+    this.multipleFileChangedHandler = this.multipleFileChangedHandler.bind(this);
+  }
+
+ 
+
+multipleFileChangedHandler = (event) => {
+  this.setState({
+   selectedFiles: event.target.files
+  });
+  console.log( "xx" + event.target.files );
+ };
+
+ 
+ 
+multipleFileUploadHandler = () => {
+  const data = new FormData();
+let selectedFiles = this.state.selectedFiles;
+// If file selected
+  if ( selectedFiles ) {
+   for ( let i = 0; i < selectedFiles.length; i++ ) {
+    data.append( 'galleryImage', selectedFiles[ i ], selectedFiles[ i ].name );
+   }
+axios.post( '/api/profile/multiple-file-upload', data, {
+    headers: {
+     'accept': 'application/json',
+     'Accept-Language': 'en-US,en;q=0.8',
+     'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+    }
+   })
+    .then( ( response ) => {
+     console.log( 'res >>>>',  response.data.locationArray );
+     this.setState({ list: response.data.locationArray});
+     console.log("State list   "+this.state.list)
+if ( 200 === response.status ) {
+      // If file size is larger than expected.
+      if( response.data.error ) {
+       if ( 'LIMIT_FILE_SIZE' === response.data.error.code ) {
+        this.ocShowAlert( 'Max size: 4MB', 'red' );
+       } else if ( 'LIMIT_UNEXPECTED_FILE' === response.data.error.code ){
+        this.ocShowAlert( 'Max 10 images allowed', 'red' );
+       } else {
+        // If not the given ile type
+        this.ocShowAlert( response.data.error, 'red' );
+       }
+      } else {
+       // Success
+       this.ocShowAlert( 'File Uploaded', '#3089cf' );
+      
+       return response.data;    
+      //  console.log("ssss "+this.state.selectedFiles.locationArray)
+      
+}
+     }
+    }).catch( ( error ) => {
+    // If another error
+    this.ocShowAlert( error, 'red' );
+   });
+  } else {
+   // if file not selected throw error
+   this.ocShowAlert( 'Please upload file', 'red' );
+  }
+};
+ 
+ 
+
+// ShowAlert Function
+ ocShowAlert = ( message, background = '#3089cf' ) => {
+      let alertContainer = document.querySelector( '#oc-alert-container' ),
+      alertEl = document.createElement( 'div' ),
+      textNode = document.createTextNode( message );
+      alertEl.setAttribute( 'class', 'oc-alert-pop-up' );
+      $( alertEl ).css( 'background', background );
+      alertEl.appendChild( textNode );
+      alertContainer.appendChild( alertEl );
+      setTimeout( function () {
+      $( alertEl ).fadeOut( 'slow' );
+      $( alertEl ).remove();
+      }, 3000 );
+ };
+render() {
+  //const items = this.state.list.map(item => <li>{item}</li> );
+
+  return(
+   <div>
+    <div className="container">
+     {/* For Alert box*/}
+     <div id="oc-alert-container"></div>
+ 
+{/* Multiple File Upload */}
+     <div className="card border-light mb-3" style={{ boxShadow: '0 5px 10px 2px rgba(195,192,192,.5)' }}>
+      <div className="card-header">
+       <h3 style={{ color: '#555', marginLeft: '12px' }}>AH DECALS : Image Utility Tool</h3>
+       <p className="text-muted" style={{ marginLeft: '12px' }}>Image Size: Max 4MB  | Max Number of Selection : 10 Images </p>
+      </div>
+      <div className="card-body">
+       <p className="card-text">Please upload the Product Images for your listing</p>
+       <input type="file" multiple onChange={this.multipleFileChangedHandler}/>
+       <div className="mt-5">
+        <button className="btn btn-info" onClick={this.multipleFileUploadHandler}>Upload!</button>
+       </div>
+      </div>
+     </div>
+</div>
+    <div className="container">
+    <div className="card border-light mb-3" style={{ boxShadow: '0 5px 10px 2px rgba(195,192,192,.5)' }}>
+      <ul>       
+        {this.state.list.map( (item,i) => (
+         <li key={i}>{item}</li>
+      ))}
+      </ul>
+      </div>
+    </div>
+   </div>
+  );
+ }
+}
+export default Home;
